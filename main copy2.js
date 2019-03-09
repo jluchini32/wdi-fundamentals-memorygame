2bf119e7da6931ae66d6473bdabc9bfb2d83bc83
jluchini32 <34019990+jluchini32@users.noreply.github.com>
2019-03-09 13:44:29 -1000

    Add files via upload

--- main copy2.js
+++ main copy2.js
@@ -3,20 +3,30 @@ console.log("Up and running!");
 var cards = ["queen" , "queen" , "king" , "king"]
 
 var cardsInPlay = ['cardOne', 'cardTwo', 'cardThree', 'CardFour'];
+cardsInPlay.push("cards[cardId]");
 
+var checkForMatch =function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+}
+}
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
 if (cardsInPlay.length === 2) {
 console.log("You found a match!");
+
 } else if (cardsInPlay[0] === cardsInPlay[1]) {
 window.alert("You found a match!");
+
 } else if (cardsInPlay[0] != cardsInPlay[1]) {
 window.alert("Sorry, try again");
 }
+
